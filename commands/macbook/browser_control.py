e9bac95928fa1529d475256ac2c84d21a38b09e2
OlegBondarUA <Petrovichteh@gmail.com>
2024-08-20 14:13:01 +0300

    Enhanced browser control to prioritize default browser if multiple are open

--- commands/macbook/browser_control.py
+++ commands/macbook/browser_control.py
@@ -3,12 +3,17 @@ import psutil
 
 
 def open_browser():
+    default_browser = "Google Chrome"
     browsers = ["Google Chrome", "Safari", "Firefox"]
+
+    opened_browser = None
 
     for process in psutil.process_iter(['name']):
         if process.info['name'] in browsers:
+            opened_browser = process.info['name']
             print(f"{process.info['name']} вже відкрито.")
-            return
+            if process.info['name'] == default_browser:
+                return
 
-    print("Відкриваю браузер...")
+    print(f"Відкриваю браузер за замовчуванням: {default_browser}")
     webbrowser.open_new_tab("https://www.google.com")
